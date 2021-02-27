aa7c36b3a0012b8c5ef3a595f35a91f2da4ed5b5
keisukekashimura <kashimura@refcome.com>
2021-02-28 00:21:10 +0900

    made it possible to ask questions

--- pages/users/[uid].tsx
+++ pages/users/[uid].tsx
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import { User } from "../../models/User";
 import Layout from "../../components/Layout";
 
@@ -10,6 +11,8 @@ type Query = {
 
 export default function UserShow() {
   const [user, setUser] = useState<User>(null);
+  const [body, setBody] = useState("");
+  const [isSending, setIsSending] = useState(false);
   const router = useRouter();
   const query = router.query as Query;
 
@@ -37,6 +40,33 @@ export default function UserShow() {
     loadUser();
   }, [query.uid]);
 
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsSending(true);
+
+    await firebase.firestore().collection("questions").add({
+      senderUid: firebase.auth().currentUser.uid,
+      receiverUid: user.uid,
+      body,
+      isReplied: false,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setIsSending(false);
+
+    setBody("");
+    toast.success("質問を送信しました。", {
+      position: "bottom-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
   return (
     <Layout>
       {user && (
@@ -45,6 +75,32 @@ export default function UserShow() {
           <div className="m-5">{user.name}さんに質問しよう！</div>
         </div>
       )}
+
+      <div className="row justify-content-center mb-3">
+        <div className="col-12 col-md-6">
+          <form onSubmit={onSubmit}>
+            <textarea
+              className="form-control"
+              placeholder="おげんきですか？"
+              rows={6}
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+              required
+            />
+            <div className="m-3">
+              {isSending ? (
+                <div className="spinner-border text-secondary" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              ) : (
+                <button type="submit" className="btn btn-primary">
+                  質問を送信する
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
     </Layout>
   );
 }
